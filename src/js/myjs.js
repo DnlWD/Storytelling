c98662ddfc23afdea1f9794449372b82399c56a2
DnlWD <dnlpmp@gmail.com>
2017-02-12 01:15:15 +0100

    audio e script
    Inserito soundtrack e controllo accesso spento con JS, iniziato a
    lavorare sulle transizioni e lo spostamento orizontale.

--- src/js/myjs.js
+++ src/js/myjs.js
@@ -1,20 +1,48 @@
+//controllo audio JS puro
+var audio = document.getElementById('background_audio');
+var tag = document.getElementById('mute')
+tag.addEventListener('click', function (e) {
+    e = e || window.event;
+    audio.muted = !audio.muted;
+    if (tag.innerHTML == "on") {
+        tag.innerHTML = "off";
+    }
+    else {
+        tag.innerHTML = "on";
+    }
+    e.preventDefault();
+}, false);
+//passiamo allo scroll "Orizzontale"
 $(document).ready(function () {
-    var transiction = window.innerHeight;
-    var larghezza = window.innerWidth;
-    var $horizontal = $('#storia');
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         $('#landing').css({
             'opacity': (100 - scroll) / 100
         });
-        if ($(window).scrollTop() > (100 - scroll)) {
-            var s = $(this).scrollTop()
-                , d = $(document).height()
-                , c = $(this).height();
-            scrollPercent = (s / (d - c));
-            var position = (scrollPercent * ($(document).width() - $horizontal.width()));
-            $horizontal.css({
-                'left': position
+        if (scroll > 100) {
+            $('#storia').css({
+                position: 'fixed'
+            });
+            $('#uno').css({
+                'margin-left': 200 - scroll
+            });
+        }
+        if (scroll > 150) {
+            $('.dialog').css({
+                'opacity': scroll / 400
+            });
+        }
+        if (scroll > 500) {
+            $('#duea').css({
+                'opacity': scroll / 1000
+            });
+            $('#dueb').css({
+                'opacity': scroll / 2000
+            });
+        }
+        if (scroll > 7000) {
+            $('#storia').css({
+                position: 'static'
             });
         }
     })
